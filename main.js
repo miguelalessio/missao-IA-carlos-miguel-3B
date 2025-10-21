7d9d7cd406129d0a4994c1f671cf91d1087a0962
miguelalessio <145574803+miguelalessio@users.noreply.github.com>
2025-10-21 11:58:16 -0300

    21/10

--- main.js
+++ main.js
@@ -75,10 +75,18 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = " ";
 
 function mostraPergunta(){
+
+   if (atual.>= perguntas.length){
+    mostraResultado();
+    return;
+   }
+    
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 
@@ -86,7 +94,7 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
@@ -94,10 +102,15 @@ function mostraAlternativas(){
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal+= afirmacoes + " ";
-    atual++
+    atual++;
     mostraPergunta();
 }
 
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se fosse possível ...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = " ";
+}
 
 mostraPergunta();
 
